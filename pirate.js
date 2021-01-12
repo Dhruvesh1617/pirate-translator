0c025bce87762107b147385eca4a45dd80e43eb9
Dhruvesh1617 <shettydhruvesh16@gmail.com>
2021-01-12 08:49:35 +0530

    update:functionality

---
+++ pirate.js
@@ -0,0 +1,34 @@
+var Text_input=document.querySelector("#txtinput");
+var Button_translate=document.querySelector("#Translate");
+var Text_output=document.querySelector("txtoutput");
+var url="https://api.funtranslations.com/translate/pirate.json";
+
+function Urlget(text)
+{
+    return url+"?"+"text="+text;
+}
+
+function Translate()
+{
+    var inputval=Text_input.value
+    fetch(Urlget(inputval))
+    .then(response=>response.json())
+    .then(json=>
+    {
+        var translate=json.contents.translated;
+        Text_output.innerText=translate;
+    
+})
+.catch(ErrorHandler);
+}
+function ErrorHandler(error)
+{
+    console.log("error occured",error);
+    alert("something went wrong");
+}
+
+
+
+
+
+Button_translate.addEventListener("click",Translate);
